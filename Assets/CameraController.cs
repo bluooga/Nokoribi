30b8eac2132b0ba71dc1b18eb754d0d463062fef
bluooga <fabiandcool17807@gmail.com>
2019-09-03 18:57:49 -0700

    first commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public bool lockCursor;
+	[SerializeField] [Range(0,20)] public float mouseSensitivity;
+	public Transform target;
+	[SerializeField] [Range(0,20)]public float dstFromTarget;
+	public Vector2 pitchMinMax = new Vector2 (-40, 85);
+
+	[SerializeField] [Range(0,1)]public float rotationSmoothTime;
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+
+	float yaw;
+	float pitch;
+
+	void Start() {
+		if (lockCursor) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+
+	void LateUpdate () {
+		yaw += Input.GetAxis ("Mouse X") * mouseSensitivity;
+		pitch -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
+
+		currentRotation = Vector3.SmoothDamp (currentRotation, new Vector3 (pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+		transform.eulerAngles = currentRotation;
+
+		transform.position = target.position - transform.forward * dstFromTarget;
+
+	}
+
+}
